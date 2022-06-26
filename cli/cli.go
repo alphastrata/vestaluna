7691c99d5249ea780ced0188edb35efa63c4044a
jer <alphastrata@gmail.com>
2022-06-26 15:41:44 +0800

    Fix: working on the cli tool (keeping in mind that NASA has a ratelimit per hour on downloads...

--- cli/cli.go
+++ cli/cli.go
@@ -2,18 +2,16 @@ package main
 
 import (
 	"fmt"
-	"log"
 	"os"
 	"strconv"
 	"strings"
 	"vestaluna/tools"
 	"vestaluna/wmts"
-
-	"fyne.io/fyne/v2/widget"
 )
 
 type Args struct {
 	mode string //concat or fetch
+	xml  string //Use when fetching
 	lod  string //LOD to scrape until, or concat at
 	dir  string //directory to store files in
 }
@@ -23,6 +21,7 @@ func parseArgs() Args {
 	if len(args) == 1 {
 		fmt.Println("Usage: ./main.go <mode> <lod> <dir>")
 		fmt.Println("mode: fetch or concat")
+		fmt.Println("xml: there, many options, copy and paste one of the below")
 		fmt.Println("lod: LOD to scrape until, or concat at")
 		fmt.Println("dir: directory to store files in")
 		fmt.Println("Databases I have information for:")
@@ -42,13 +41,16 @@ func parseArgs() Args {
 
 	if args[1] == "fetch" {
 		return Args{mode: args[1],
-			lod: args[2],
-			dir: ""} //N/A
+			lod: args[3],
+			dir: "",
+			xml: args[2]}
 	}
 
 	return Args{mode: args[1],
-		lod: args[2],
-		dir: args[3]} // Location of dataset from which to concat
+		lod: args[3],
+		dir: args[4],
+		xml: args[2],
+	}
 }
 
 var XML = []string{"https://trek.nasa.gov/tiles/Moon/EQ/LRO_LOLA_Shade_Global_128ppd_v04/1.0.0/WMTSCapabilities.xml",
@@ -60,19 +62,25 @@ var XML = []string{"https://trek.nasa.gov/tiles/Moon/EQ/LRO_LOLA_Shade_Global_12
 	"https://trek.nasa.gov/tiles/Mars/EQ/Mars_MOLA_blend200ppx_HRSC_ClrShade_clon0dd_200mpp_lzw/1.0.0/WMTSCapabilities.xml"}
 
 func main() {
-	wmtsXML := XML[3]
 	args := parseArgs()
+	var wmtsXML string
+
+	for _, s := range XML {
+		if strings.Contains(s, args.dir) {
+			wmtsXML = s
+		}
+
+	}
+
 	switch args.mode {
 	case "fetch":
 		// remove the misses.txt
 		os.Remove("misses.txt")
-		uiPbar := widget.NewProgressBar() //NOTE: Never used just passing as a dummy, may need to address later..
 		lod, _ := strconv.Atoi(args.lod)
-		wmts.FetchExact(wmtsXML, lod, uiPbar)
+		wmts.FetchExact(wmtsXML, lod)
 	case "concat":
 		sp := strings.Split(wmtsXML, "/")
 		dirpath := "downloads/" + sp[len(sp)-2]
-		log.Println("CLI>>> Dirpath is:", dirpath)
 		lod, _ := strconv.Atoi(args.lod)
 		tools.ConcatWithPython(dirpath, lod)
 	}
